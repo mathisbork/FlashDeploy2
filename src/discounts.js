6b70eb1f13b7eeb7a7940ed9702c8290441a9195
Grégoire PATIN <greg.pat.dev@gmail.com>
2026-06-04 16:12:54 +0200

    feat(discount): add SUMMER25 season check (WIP)

--- src/discounts.js
+++ src/discounts.js
@@ -6,3 +6,10 @@ function applyCode(code, amount) {
   return amount - (amount * d.percent / 100);
 }
 module.exports = { registerCode, applyCode, CODES };
+
+// SUMMER25 - work in progress, ne pas activer
+function summerSeasonActive(date) {
+  const m = date.getMonth();
+  return m >= 5 && m <= 7;
+}
+module.exports.summerSeasonActive = summerSeasonActive;
