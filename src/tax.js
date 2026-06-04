08036f67461253e067db6d94a5d8ee186fad6ce3
Peti0u <quent1.vibrac@gmail.com>
2026-06-04 16:45:40 +0200

    fix: tva rounding

--- src/tax.js
+++ src/tax.js
@@ -1,5 +1,5 @@
 // BUG: Math.floor introduit une erreur d'arrondi sur les gros montants
 function computeVAT(amount, rate) {
-  return Math.floor(amount * rate * 100) / 100;
+  return Math.round(amount * rate * 100) / 100;
 }
 module.exports = { computeVAT };
