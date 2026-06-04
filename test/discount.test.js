befdc46043aa38ad41bb7945e011b2da009936f3
Grégoire PATIN <greg.pat.dev@gmail.com>
2026-06-04 16:12:54 +0200

    test(discount): placeholder for SUMMER25 (WIP)

--- test/discount.test.js
+++ test/discount.test.js
@@ -2,3 +2,4 @@ const { applyCode } = require('../src/discounts');
 const r = applyCode('WELCOME10', 100);
 if (r !== 90) { console.error('FAIL WELCOME10', r); process.exit(1); }
 console.log('OK WELCOME10');
+// TODO: test SUMMER25 quand la logique saison sera validee
