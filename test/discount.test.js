4dff70bd78ded47cce4e6b7cd387f002f95776ff
Peti0u <quent1.vibrac@gmail.com>
2026-06-05 11:07:52 +0200

    docs: WELCOME10 & hotfixv1.2.1 on CHANGELOG.md

--- test/discount.test.js
+++ test/discount.test.js
@@ -1,4 +1,7 @@
-const { applyCode } = require('../src/discounts');
-const r = applyCode('WELCOME10', 100);
-if (r !== 90) { console.error('FAIL WELCOME10', r); process.exit(1); }
-console.log('OK WELCOME10');
+const { applyCode } = require("../src/discounts");
+const r = applyCode("WELCOME10", 100);
+if (r !== 90) {
+  console.error("FAIL WELCOME10", r);
+  process.exit(1);
+}
+console.log("OK WELCOME10");
